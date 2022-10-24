1f624a3cde61e791bbe3ccebcc7cff2e7dfbf8cf
Hrvoje <hrvojehemen@gmail.com>
2022-10-24 18:17:48 +0200

    moved json to public folder

--- pages/api/addMatch.js
+++ pages/api/addMatch.js
@@ -4,10 +4,10 @@ import { promises as fs } from 'fs';
 export default async function handler(req, res) {
     //Find the absolute path of the json directory
     //Read the json data file data.json
-    const fileContents = await fs.readFile('data/matches.json', 'utf8');
+    const fileContents = await fs.readFile('public/data/matches.json', 'utf8');
     let obj = JSON.parse(fileContents)
     obj.push({"bla":"bla"})
-    await fs.writeFile('data/matches.json', JSON.stringify(obj, null, 4));
+    await fs.writeFile('public/data/matches.json', JSON.stringify(obj, null, 4));
 
     res.status(200).json(obj);
 }
--- pages/api/getMatches.js
+++ pages/api/getMatches.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
     //Read the json data file data.json
-    const fileContents = await fs.readFile('data/matches.json', 'utf8');
+    const fileContents = await fs.readFile('public/data/matches.json', 'utf8');
     //Return the content of the data file in json format
     let obj = JSON.parse(fileContents)
     res.status(200).json(obj);

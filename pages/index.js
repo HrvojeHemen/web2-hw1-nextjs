d7729694ce52e9a1af07e76092afe40740593643
Hrvoje <hrvojehemen@gmail.com>
2022-10-27 18:11:49 +0200

    state test

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import {useUser} from "@auth0/nextjs-auth0";
-import {HStack, VStack} from "@chakra-ui/react";
-import { Link } from '@chakra-ui/react'
+import {HStack, Text, VStack} from "@chakra-ui/react";
+import {Link} from '@chakra-ui/react'
+import {useEffect, useState} from "react";
+import {supabase} from "../db/supabaseConnector";
 
-export default function Home() {
+export default function Home () {
     const {user} = useUser();
+    const [matches, setMatches] = useState([])
+
+    useEffect(async () => {
+        let matchesFromDb, err = await supabase
+            .from('matches')
+            .select()
+
+        setMatches(matchesFromDb)
+
+    }, [])
+
     return (
         <VStack>
             <HStack>
-                <Link href="/api/auth/login" > LOG IN </Link>
+                <Link href="/api/auth/login"> LOG IN </Link>
                 <Link href="/api/auth/logout"> LOG OUT</Link>
             </HStack>
-
-            <div>
+            <Text>
                 {JSON.stringify(user)}
-            </div>
+            </Text>
+            <Text>
+                {JSON.stringify(matches)}
+            </Text>
         </VStack>
 
     )
